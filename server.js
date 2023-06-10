0f103512b72cbfc9e507a92c009fa7b0f741ac0b
Albertamoahjunior <49646634+Albertamoahjunior@users.noreply.github.com>
2023-06-10 16:08:10 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+//Main working code come in here
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`shop running on port ${PORT}`);
+})
---
+++ src/db.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user:"postgres",
+    host:"127.0.0.1",
+    database:"shopcomany",
+    password:"admin",
+    port:"5432"
+});
+
+
+module.exports = pool;
