3d5ba3b1e20ddf2436f5ed98b7d176f8c8fc223f
Albertamoahjunior <49646634+Albertamoahjunior@users.noreply.github.com>
2023-07-09 02:15:41 +0000

    more

--- frontend/manager/manager-app/src/customers.js
+++ frontend/manager/manager-app/src/customers.js
@@ -3,6 +3,11 @@ import Navbar from './navbar';
 import GreetTag from './welcome-tag';
 
 function Customer (){
+    const customer_list = () =>{
+        let customer = ["Kofi", "Ama", "Kwame"];
+        
+        return customer;
+    }
     return(
         <div className='customers'>
             <Navbar/>
@@ -11,7 +16,11 @@ function Customer (){
                 </div>
             <div className='customer-content'>
                 <div className='customer-rep'>
-
+                    {
+                        customer_list().map((item, index) =>(
+                            <button>{item}</button>
+                        ))
+                    }
                 </div>
                 <div className='customer-details'>
                     <GreetTag greeting="Customer Details"/>
